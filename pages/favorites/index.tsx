1b7ae50fda763c6ee2e8265477b8b49843c9a819
Jorge Luis Adco Ccahuay <jorge.adco@whiz.one>
2022-05-15 13:55:14 -0500

    add list pokemon favorites

--- pages/favorites/index.tsx
+++ pages/favorites/index.tsx
@@ -1,18 +1,48 @@
+import { Card, Grid } from '@nextui-org/react';
 import { NextPage } from 'next';
+import { Router, useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { Layout } from '../../components/layouts';
 import { NoFavorites } from '../../components/ui/NoFavorites';
 import { localFavorites } from '../../utils';
 
 const FavoritesPage: NextPage = () => {
+  const router = useRouter();
   const [favoritePokemons, setFavoritePokemons] = useState<number[]>([]);
   useEffect(() => {
     setFavoritePokemons(localFavorites.pokemons());
   }, []);
-
+  const onPokemonClick = (id: number) => {
+    router.push(`/pokemon/${id}`);
+  };
   return (
     <Layout title="Pokemon - Favoritos">
-      <NoFavorites />
+      {favoritePokemons.length === 0 ? (
+        <NoFavorites />
+      ) : (
+        <Grid.Container gap={2} justify="flex-start" direction="row">
+          {favoritePokemons.map((id) => (
+            <Grid
+              key={id}
+              xs={6}
+              sm={3}
+              md={2}
+              xl={1}
+              onClick={() => {
+                onPokemonClick(id);
+              }}
+            >
+              <Card hoverable clickable css={{ padding: 10 }}>
+                <Card.Image
+                  src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`}
+                  width={'100%'}
+                  height="140px"
+                />
+              </Card>
+            </Grid>
+          ))}
+        </Grid.Container>
+      )}
     </Layout>
   );
 };
