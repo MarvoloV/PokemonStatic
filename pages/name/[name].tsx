c6b0945c7fb21ff629edad47334bb914f12747af
Jorge Luis Adco Ccahuay <jorge.adco@whiz.one>
2022-05-15 17:08:35 -0500

    add page PokemonByNamePage path: http://localhost:3000/name/[namePokemon]

---
+++ pages/name/[name].tsx
@@ -0,0 +1,139 @@
+import { useEffect, useState } from 'react';
+import { NextPage, GetStaticProps, GetStaticPaths } from 'next';
+import { Grid, Card, Button, Container, Text, Image } from '@nextui-org/react';
+import confetti from 'canvas-confetti';
+import { Layout } from '../../components/layouts';
+import { Pokemon, PokemonListResponse } from '../../interfaces';
+import { localFavorites } from '../../utils';
+import { pokeapi } from '../../api';
+
+interface Props {
+  pokemon: Pokemon;
+}
+
+const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
+  const [isInFavorites, setIsInFavorites] = useState(false);
+  useEffect(() => {
+    setIsInFavorites(localFavorites.isExistPokemoninFavorites(pokemon.id));
+  }, [pokemon.id]);
+  const onTogleFavorite = () => {
+    localFavorites.togleFavorite(pokemon.id);
+    setIsInFavorites(!isInFavorites);
+    if (isInFavorites) {
+      return;
+    }
+    confetti({
+      zIndex: 999,
+      particleCount: 100,
+      spread: 160,
+      angle: -100,
+      origin: {
+        x: 1,
+        y: 0,
+      },
+    });
+  };
+  return (
+    <Layout title={pokemon.name}>
+      <Grid.Container css={{ marginTop: '5px' }} gap={2}>
+        <Grid xs={12} sm={4}>
+          <Card hoverable css={{ padding: '30px' }}>
+            <Card.Body>
+              <Card.Image
+                src={
+                  pokemon.sprites.other?.dream_world.front_default ||
+                  '/no-image.png'
+                }
+                alt={pokemon.name}
+                width="100%"
+                height={200}
+              />
+            </Card.Body>
+          </Card>
+        </Grid>
+        <Grid xs={12} sm={8}>
+          <Card>
+            <Card.Header
+              css={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                '@xs': {
+                  flexDirection: 'row',
+                  justifyContent: 'space-between',
+                },
+              }}
+            >
+              <Text h1 transform="capitalize">
+                {pokemon.name}
+              </Text>
+              <Button
+                color="gradient"
+                ghost={!isInFavorites}
+                onClick={onTogleFavorite}
+              >
+                {isInFavorites ? 'En Favoritos' : 'Guardar en Favoritos'}
+              </Button>
+            </Card.Header>
+            <Card.Body>
+              <Text size={30}>Sprites:</Text>
+              <Container display="flex" direction="row">
+                <Image
+                  src={pokemon.sprites.front_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_default}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.front_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+                <Image
+                  src={pokemon.sprites.back_shiny}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </Container>
+            </Card.Body>
+          </Card>
+        </Grid>
+      </Grid.Container>
+    </Layout>
+  );
+};
+// You should use getStaticProps when:
+//- The data required to render the page is available at build time ahead of a user’s request.
+//- The data comes from a headless CMS.
+//- The data can be publicly cached (not user-specific).
+//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
+// You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
+
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const { data } = await pokeapi.get<PokemonListResponse>('/pokemon?limit=151');
+  const pokemonNames: string[] = data.results.map((pokemon) => pokemon.name);
+  return {
+    paths: pokemonNames.map((name) => ({ params: { name } })),
+    fallback: false,
+  };
+};
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { name } = params as { name: string };
+  const { data } = await pokeapi.get(
+    `https://pokeapi.co/api/v2/pokemon/${name}`
+  );
+  return {
+    props: {
+      pokemon: data,
+    },
+  };
+};
+export default PokemonByNamePage;
